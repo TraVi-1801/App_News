2f4b99f5d9e3bad61020609a1a19f15e9b36e44f
Nguyễn Trà Vi <vi.nt@intelin.vn>
2025-07-22 17:58:11 +0700

    test: add comment in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/vic/project/app_news/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vic/project/app_news/ExampleInstrumentedTest.kt
@@ -18,6 +18,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
+        //test
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.vic.project.app_news", appContext.packageName)
     }
